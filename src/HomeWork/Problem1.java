f6c9deb15a79e87e212dbb95d89e893b1fddc489
Tien <quangtien05022001@gmail.com>
2023-11-23 20:50:53 +0700

    Test branch session 2

--- src/HomeWork/Problem1.java
+++ src/HomeWork/Problem1.java
@@ -4,9 +4,18 @@ import java.util.Scanner;
 
 public class Problem1 {
     public static void main(String[] args) {
-        System.out.println("Enter your name");
+
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Hello: "+name);
+        System.out.println("Hãy nhập khoảng chặn đầu: ");
+        int start = Integer.parseInt(scanner.nextLine());
+        System.out.println("Hãy nhập khoảng chặn đuôi: ");
+        int end = Integer.parseInt(scanner.nextLine());
+        int allEvenSum = 0;
+        for (int i = start; i < end + 1; i++) {
+            if(i%2==0){
+                allEvenSum = i+allEvenSum;
+            }
+        }
+        System.out.printf("Tổng của các số chẵn trong khoảng đã cho là: %d",allEvenSum);
     }
 }
