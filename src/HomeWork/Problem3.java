c46493f2c48d1a9207ca616641b19b65f8527197
Tien <quangtien05022001@gmail.com>
2023-11-24 12:13:28 +0700

    Check Prime

--- src/HomeWork/Problem3.java
+++ src/HomeWork/Problem3.java
@@ -1,4 +1,31 @@
 package HomeWork;
 
 public class Problem3 {
+
+
+    public static boolean checkPrimeNumber(int x) {
+        if(x<1){
+            return false;
+        } else if (x==2) {
+            return true;
+        } else if (x>2) {
+            for(int i=2;i<x;i++){
+                if(x%i==0){
+                    return false;
+
+                }
+
+                return true;
+
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        boolean isPrime = checkPrimeNumber(6);
+        System.out.println("Co phai so nguyen to hay khong ?"+ isPrime);
+
+    }
 }
